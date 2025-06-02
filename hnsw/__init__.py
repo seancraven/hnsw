72e5606838ebe73a6030dba9cf5d451378dee070
Sean Craven <sean.craven@advai.co.uk>
2025-06-02 20:52:02 +0100

    wip: select neighbours

--- hnsw/__init__.py
+++ hnsw/__init__.py
@@ -37,6 +37,62 @@ class PrioritizedNode:
         return cls(distance=-l2(n.v, query_node), n=n)
 
 
+def select_neighbours_simple(
+    query_node: vec,
+    candidate_nodes: list[Node],
+    retrival_count: int,
+    layer_id: int,
+) -> list[Node]:
+    out: list[PrioritizedNode] = []
+    for node in candidate_nodes:
+        heapq.heappush(out, PrioritizedNode.from_node(node, query_node))
+        if len(out) > retrival_count:
+            out.pop()
+    return [n.n for n in out]
+
+
+def select_neighbours_heuristic(
+    query_node: vec,
+    candidate_nodes: list[Node],
+    retrival_count: int,
+    layer_id: int,
+    extend_candidates: bool = False,
+    keep_pruned_connections: bool = False,
+) -> list[Node]:
+    candidate_queue = [
+        PrioritizedNode.from_node_inverse(n, query_node) for n in candidate_nodes
+    ]
+    candidate_vecs = {n.v for n in candidate_nodes}
+    heapq.heapify(candidate_nodes)
+    out = []
+    discarded_nodes = []
+    if extend_candidates:
+        for n in candidate_queue:
+            for neigbour in n.n.adjacent_nodes:
+                if neigbour.v in candidate_vecs:
+                    continue
+                heapq.heappush(
+                    candidate_queue,
+                    PrioritizedNode.from_node_inverse(neigbour, query_node),
+                )
+                candidate_vecs.add(neigbour.v)
+    while candidate_queue and len(out) <= retrival_count:
+        new_candidate = candidate_queue.pop()
+        new_candidate.distance = -new_candidate.distance
+        if not out:
+            heapq.heappush(out, new_candidate)
+        else:
+            closest_result = out[0]
+            if new_candidate.distance < closest_result.distance:
+                heapq.heappush(out, new_candidate)
+            else:
+                out.append(new_candidate)
+    if keep_pruned_connections:
+        # TODO:
+        pass
+    return out
+
+
 @dataclass
 class NodeLayer:
     nodes: list[Node]
